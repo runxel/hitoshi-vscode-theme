51e1a47d19b169b268eab131df5d2027015106ba
Lucas Becker <runxel@users.noreply.github.com>
2020-01-06 21:37:27 +0100

    changed demo files

--- demo/python.py
+++ demo/python.py
@@ -1,33 +1,32 @@
 from collections import deque
 
-
 def topo(G, ind=None, Q=[1]):
-  if ind == None:
-    ind = [0] * (len(G) + 1)  # this is a comment
-      for u in G:
-        for v in G[u]:
-          ind[v] += 1
-      Q = deque()
-      for i in G:
-        if ind[i] == 0:
-          Q.append(i)
-  if len(Q) == 0:
-    return
+    if ind == None:
+        ind = [0] * (len(G) + 1)  # this is a comment
+        for u in G:
+            for v in G[u]:
+                ind[v] += 1
+            Q = deque()
+            for i in G:
+                if ind[i] == 0:
+                    Q.append(i)
+    if len(Q) == 0:
+        return
 
-  v = Q.popleft()
-  print(v)
+    v = Q.popleft()
+    print(v)
 
-  for w in G[v]:
-      ind[w] -= 1
-      if ind[w] == 0:
-          Q.append(w)
-  topo(G, ind, Q)
+    for w in G[v]:
+        ind[w] -= 1
+        if ind[w] == 0:
+            Q.append(w)
+    topo(G, ind, Q)
 
 
 class SomeClass:
-  """ Describe what this is about.
-      TODO explain it more
-  """
+    """ Describe what this is about.
+        TODO explain it more
+    """
     def create_arr(self): # An instance method
         self.arr = []
 
